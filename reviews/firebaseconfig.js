7cdfb6426958278380cb3cd9adb52fa0f9f574ae
mvrv <abigaelpl.ti22@utsjr.edu.mx>
2024-10-27 23:52:52 -0600

    Update firebaseconfig.js

--- reviews/firebaseconfig.js
+++ reviews/firebaseconfig.js
@@ -1,9 +1,22 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../key.json');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: "6f0024b6c18425c1c499d94e06cfb14747e36a15", 
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: "116057195658126338851",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zip84%40edos-84ac7.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://edos-84ac7.firebaseio.com" 
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 const db = admin.firestore();
