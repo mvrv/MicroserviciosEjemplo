7c93df215da289c32d1a98969e1e0b76f9a46458
mvrv <abigaelpl.ti22@utsjr.edu.mx>
2024-10-27 23:36:52 -0600

    Update server.js

--- home/server.js
+++ home/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080; // Usa el puerto de la variable de entorno o 8080 como predeterminado
 const db = require('./firebaseconfig');
 const path = require('path');
 const cors = require('cors');
