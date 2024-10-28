b2e947baf03c43fa89c538c1555499de77348d97
mvrv <abigaelpl.ti22@utsjr.edu.mx>
2024-10-28 01:33:05 -0600

    aa

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 9001;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'views')));
